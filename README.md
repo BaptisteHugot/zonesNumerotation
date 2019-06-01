aa165d685d4300453d8fffeab912f30f4517dc79
Baptiste Hugot <baptistehugot@gmail.com>
2019-06-01 17:45:13 +0200

    Initial work

---
+++ README.md
@@ -0,0 +1,30 @@
+# zonesNumerotation
+Ce programme affiche une carte montrant les différentes zones de numérotation en France, suivant trois niveaux distincts :
+* Au niveau de la zone géographique (Z)
+* Au niveau de la zone de numérotation (ZAB)
+* Au niveau de la zone élémentaire de numérotation (ZNE)
+Les données sont basées sur les fichiers disponibles sur le site de l'[Arcep](https://extranet.arcep.fr/portail/Op%C3%A9rateursCE/Num%C3%A9rotation.aspx), le régulateur des communications électroniques en France.
+
+## Exemple
+Un exemple complet est disponible sur [ma page personnelle GitHub](https://baptistehugot.github.io/zonesNumerotation/index.html).
+
+## Ecrit avec
+* [Javascript](https://www.ecma-international.org/publications/standards/Ecma-262.htm) - Le langage de programmation utilisé pour gérer la gestion de la carte et des bibliothèques
+* [HTML](https://www.w3.org/html/) - Le langage de programmation utilisé pour afficher la page Internet
+* [CSS](https://www.w3.org/Style/CSS/) - Le langage de programmation utilisé pour gérer les styles de la page Internet
+
+## Bibliothèques utilisées
+* [Leaflet](https://leafletjs.com/) - La bibliothèque utilisée pour afficher la carte
+* [Leaflet-Ajax](https://github.com/calvinmetcalf/leaflet-ajax) - La bibliothèque utilisée pour gérer l'affichage des zones contenues dans le fichier GeoJson
+* [Leaflet-Geosearch](https://github.com/smeijer/leaflet-geosearch) - La bibliothèque utilisée pour permettre la recherche par commune
+* [Leaflet-Locatecontrol](https://github.com/domoritz/leaflet-locatecontrol) - La bibliothèque utilisée pour permettre la géolocalisation de l'utilisateur
+* [Leaflet.fullscreen](https://github.com/Leaflet/Leaflet.fullscreen) - La bibliothèque utilisée pour afficher la carte en plein écran
+
+## Versions
+[SemVer](http://semver.org/) est utilisé pour la gestion de versions. Pour connaître les versions disponibles, veuillez vous référer aux [étiquettes disponibles dans ce dépôt](https://github.com/BaptisteHugot/arretRTC/releases/).
+
+## Auteurs
+* **Baptiste Hugot** - *Travail initial* - [BaptisteHugot](https://github.com/BaptisteHugot)
+
+## Licence
+Ce projet est disponible sous licence MIT. Veuillez lire le fichier [LICENSE.md](LICENSE.md) pour plus de détails.
---
+++ carte.js
@@ -0,0 +1,239 @@
+/**
+ * Fichier générant la carte qui sera affichée montrant les différentes zones de numérotation en France, par types de zones
+ */
+
+// Initialisation des variables
+var latitude = 46.49389;
+var longitude = 2.602778;
+var map = null;
+var GeoSearchControl = window.GeoSearch.GeoSearchControl;
+var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+var zLayer;
+var zabLayer;
+var zneLayer;
+var info;
+var control;
+
+/**
+ * Fonction définissant la couleur qui est affectée à une zone particulière
+ * @param L'identifiant de la couleur concernée
+ */
+function getColor(id) {
+    return id == 1 ? '#E41A1C' :
+        id == 2 ? '#377EB8' :
+        id == 3 ? '#4DAF4A' :
+        id == 4 ? '#984EA3' :
+        id == 5 ? '#FF7F00' :
+        id == 6 ? '#FFFF33' :
+        '#D8B2D8';
+}
+
+/**
+ * Fonction qui définit le style qui sera affiché en fonction d'une donnée particulière
+ * @param Le fichier GeoJson en entrée
+ */
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.color_id),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+/*
+ * Foncion qui définit un listener pour lorsque la souris est sur une des couches
+ * @param Évènement
+ */
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+/*
+ * Fonction qui définit ce qui se passe lorsque la souris quitte une des couches
+ * @param Évènement
+ */
+function resetHighlight(e) {
+    zLayer.resetStyle(e.target);
+    zabLayer.resetStyle(e.target);
+    zneLayer.resetStyle(e.target);
+    info.update();
+}
+
+/*
+ * Fonction qui va permettre de zoomer sur l'élément lorsqu'un clic est détecté sur ce dernier
+ * @param Évènement
+ */
+function zoomToFeature(e) {
+    macarte.fitBounds(e.target.getBounds());
+}
+
+/**
+ * Fonction qui ajoute les listeners pour chaque couche de la carte
+ * @param Fonctionnalités
+ * @param Couches
+ */
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        //click: zoomToFeature
+    });
+}
+
+/**
+ * Fonction qui déplace des éléments au format HTML dans un nouveau parent
+ * @param Eléments à déplacer
+ * @param Le nouvel élément où l'élément "el" sera déplacé
+ */
+function setParent(el, newParent) {
+    newParent.appendChild(el);
+}
+
+/**
+ * Fonction d'initialisation de la carte
+ */
+function initMap() {
+    // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    map = L.map('map', {
+        fullscreenControl: {
+            pseudoFullscreen: false, // if true, fullscreen to page width and height
+            position: 'topleft'
+        }
+    }).setView([latitude, longitude], 6);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        minZoom: 1,
+        maxZoom: 20
+    });
+
+    // On lit les données contenues dans les fichiers geojson
+    zLayer = new L.GeoJSON.AJAX("./Donnees/carte_z.json", {
+        style: style,
+        onEachFeature: onEachFeature
+    });
+    zabLayer = new L.GeoJSON.AJAX("./Donnees/carte_zab.json", {
+        style: style,
+        onEachFeature: onEachFeature
+    });
+    zneLayer = new L.GeoJSON.AJAX("./Donnees/carte_zne.json", {
+        style: style,
+        onEachFeature: onEachFeature
+    });
+
+    var overlayMaps = {
+        "Zones géographiques (Z)": zLayer,
+        "Zones de numérotation (ZAB)": zabLayer,
+        "Zones de numérotation élémentaire (ZNE)": zneLayer
+    };
+
+    control = L.control.layers(overlayMaps, null, {
+        collapsed: false
+    }); // On ajoute les trois couches précédentes à la carte, en les rendant exclusives
+
+    // On gère la géolocalisation de l'utilisateur
+    var location = L.control.locate({
+        position: 'topleft',
+        setView: 'untilPanOrZoom',
+        flyTo: false,
+        cacheLocation: true,
+        drawMarker: true,
+        drawCircle: false,
+        showPopup: false,
+        keepCurrentZoomLevel: true
+    });
+
+    // On définit le fournisseur sur lequel on va s'appuyer pour effectuer les recherches d'adresse
+    var provider = new OpenStreetMapProvider({
+        params: {
+            countrycodes: 'fr'
+        }, // On restreint uniquement les recherches pour la France
+    });
+
+    // On définit le module de recherche
+    var searchControl = new GeoSearchControl({
+        provider: provider,
+        showMarker: true,
+        showPopup: false,
+        marker: {
+            icon: new L.Icon.Default,
+            draggable: false,
+            interactive: false
+        },
+        maxMarkers: 1,
+        retainZoomLevel: true,
+        animateZoom: true,
+        autoClose: true,
+        searchLabel: "Entrez l'adresse",
+        keepResult: true
+    });
+
+    info = L.control();
+
+    /**
+     * On ajoute les informations à la carte
+     * @param la carte où les informations seront ajoutées
+     */
+    info.onAdd = function(map) {
+        this._div = L.DomUtil.create('div', 'info'); // On créé une section avec la classe info
+        this.update();
+        return this._div;
+    }
+
+    /**
+     * Fonction qui met à jour les informations en fonction des propriétés passées
+     * @param Propriétés
+     */
+    info.update = function(props) {
+        this._div.innerHTML = '<h4>Zone de numérotation</h4>' + (props ?
+            '<b>' + props.DECOUPAGE : '<br />');
+    };
+
+    // On ajoute toutes les couches à la carte
+    control.addTo(map);
+    osmLayer.addTo(map);
+    zLayer.addTo(map);
+    map.addControl(searchControl);
+    location.addTo(map);
+    info.addTo(map);
+
+    // On met les boutons radio servant à contrôler la carte qui sera affichée en dehors de la carte
+    var htmlObject = control.getContainer();
+    var a = document.getElementById('control');
+    setParent(htmlObject, a);
+
+    // On change l'endroit où s'affiche les boutons radio lorsque l'utilisateur passe en plein écran
+    map.on('fullscreenchange', function() {
+        if (map.isFullscreen()) {
+            control.addTo(map);
+        } else {
+            htmlObject = control.getContainer();
+            setParent(htmlObject, a);
+        }
+    });
+
+}
+
+/**
+ * Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+ */
+window.onload = function() {
+    initMap();
+};
